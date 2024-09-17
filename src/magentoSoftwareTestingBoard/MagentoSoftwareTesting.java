06068a43055f7fda9e8678021403dbdd79d196c3
Dania Jarad <65604677+Daniajarad97@users.noreply.github.com>
2024-09-18 00:15:34 +0300

    update

---
+++ src/magentoSoftwareTestingBoard/MagentoSoftwareTesting.java
@@ -0,0 +1,80 @@
+package magentoSoftwareTestingBoard;
+
+import java.time.Duration;
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MagentoSoftwareTesting {
+
+	WebDriver driver = new ChromeDriver();
+	Random rand = new Random();
+	String URLSite = "https://magento.softwaretestingboard.com/";
+	String password = "12345678#Test";
+
+	@BeforeTest
+	public void setup() {
+
+		driver.get(URLSite);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+
+	@Test(priority = 1, enabled = true)
+	public void createAnAccountTest() throws InterruptedException {
+
+		WebElement creatAnAccountButton = driver.findElement(By.linkText("Create an Account"));
+		creatAnAccountButton.click();
+
+		String[] firs_tNames = { "Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Helen", "Ivan", "Judy",
+				"Kathy", "Leo", "Mona", "Nina", "Oscar" };
+		String[] last_Names = { "Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis",
+				"Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson" };
+		String[] email_Address = { "@Gmail.com", "@Yahoo.com", "@Outlook.com" };
+
+		int randNamber = rand.nextInt(444);
+		int randomIndexForFirstName = rand.nextInt(firs_tNames.length);
+		int randomIndexForLastName = rand.nextInt(last_Names.length);
+		int randomIndexForEmail = rand.nextInt(email_Address.length);
+		String randFirstName = firs_tNames[randomIndexForFirstName];
+		String randLastName = last_Names[randomIndexForLastName];
+		String randEmail = email_Address[randomIndexForEmail];
+		String firstName = "firstname";
+		String lastName = "lastname";
+		String emailAddress = "email_address";
+		String passwordInput = "password";
+		String passwordConfirmation = "password-confirmation";
+
+		WebElement firstNameButtom = driver.findElement(By.id(firstName));
+		WebElement lastNameButton = driver.findElement(By.id(lastName));
+		WebElement emailButton = driver.findElement(By.id(emailAddress));
+		WebElement passwordButton = driver.findElement(By.id(passwordInput));
+		WebElement passwordConfirmationButton = driver.findElement(By.id(passwordConfirmation));
+
+		firstNameButtom.sendKeys(randFirstName);
+		lastNameButton.sendKeys(randLastName);
+		emailButton.sendKeys(randFirstName + randLastName + randNamber + randEmail);
+		passwordButton.sendKeys(password);
+		passwordConfirmationButton.sendKeys(password);
+
+		WebElement submitButton = driver.findElement(By.cssSelector("button[title='Create an Account']"));
+		submitButton.click();
+		
+		//Thread.sleep(1000);
+
+		WebElement messageAsWebElement = driver
+				.findElement(By.cssSelector("div[data-bind='html: $parent.prepareMessageForHtml(message.text)']"));
+		String actualMessage = messageAsWebElement.getText();
+		String expectedMessage = "Thank you for registering with Main Website Store.";
+		Assert.assertEquals(actualMessage, expectedMessage);
+
+	}
+	
+
+}
